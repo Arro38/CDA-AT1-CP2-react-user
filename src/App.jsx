7855526d37f446cd5a9f4c5111263fc5a31a7ac9
Etienne VAYTILINGOM <etienne.vaytilingom.pro@gmail.com>
2025-04-24 09:58:05 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const url = "https://randomuser.me/api/?results=5";
+  const [users,setUsers] = useState([]);
+
+  const getUsers = async ()=>{
+      const request = await fetch(url);
+      const data = await request.json();
+      setUsers(data.results);
+  }
+
+  useEffect(()=>{
+    getUsers();
+  },[])
+
+  return (
+    <>
+      <h1>React users</h1>
+      <ul>
+      {users
+      .sort((u1,u2)=>u1.name.last.localeCompare(u2.name.last))
+      .map((u,index)=>
+        <li key={index}>{u.name.last} {u.name.first} - {u.phone} <span className="star"> &#9734; </span></li>
+      )}
+      </ul>
+    </>
+  )
+}
+
+export default App

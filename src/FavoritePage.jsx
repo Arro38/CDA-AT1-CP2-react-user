59e84fd71c604debecd77c88b5ea7ce56e6b850c
Etienne VAYTILINGOM <etienne.vaytilingom.pro@gmail.com>
2025-04-24 14:15:10 +0400

    toggle favorite ok

--- src/FavoritePage.jsx
+++ src/FavoritePage.jsx
@@ -1,14 +1,21 @@
 import User from "./User";
 
-function FavoritePage({ users }) {
+function FavoritePage({ users, setUsers }) {
   return (
     <>
       <h1>Favorite</h1>
       <ul>
         {users
+          .filter((u) => u.favorite)
           .sort((u1, u2) => u1.name.last.localeCompare(u2.name.last))
           .map((u, index) => (
-            <User key={index} user={u} i={index} />
+            <User
+              key={index}
+              user={u}
+              i={index}
+              users={users}
+              setUsers={setUsers}
+            />
           ))}
       </ul>
     </>
--- src/User.jsx
+++ src/User.jsx
@@ -1,10 +1,17 @@
-function User({ user }) {
+function User({ user, users, setUsers }) {
+  function toggleFavorite() {
+    const i = users.findIndex((u) => u.id.value == user.id.value);
+    let copy = [...users];
+    copy[i].favorite = !copy[i].favorite;
+    setUsers(copy);
+  }
+
   return (
     <li>
       {user.name.last} {user.name.first} - {user.phone}
       <span
-        className="star"
-        onClick={(e) => e.target.classList.toggle("active")}
+        className={user.favorite ? "star active" : "star"}
+        onClick={(e) => toggleFavorite()}
       >
         &#9734;
       </span>
